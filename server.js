60476fac9488cc17934d0ae21bed4cb8615ada19
jocanola <jokanola.it@gmail.com>
2022-01-15 09:10:43 +0100

    initializing dua-data

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const dotenv = require("dotenv");
+
+
+const app = express();
+//Middleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+dotenv.config();
+
+
+//listening to the port
+app.listen(process.env.PORT || 5000, (req, res) => {
+  console.log("start the backend");
+});
+//Connect to mongodb
+mongoose.connect(
+  process.env.MONGODB_URL,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  () => {
+    console.log("Databaee connected");
+  }
+);
+
+app.get("/", (req, res) => {
+  res.status(200).send("Hello world make global beast come through");
+});
+
